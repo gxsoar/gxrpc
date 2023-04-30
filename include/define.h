9cacd825f10f3032618826010c953276456422bb
gxsoar <2278926493gx@gmail.com>
2023-04-30 15:12:18 +0800

    add static_max, static_min

--- include/define.h
+++ include/define.h
@@ -3,3 +3,13 @@
 #include <functional>
 
 using Task = std::function<void(void*)>;
+
+template <int A, int B>
+struct static_max {
+  static const int value = A > B ? A : B;
+};
+
+template <int A, int B>
+struct static_min {
+  static const int value = A < B ? A : B;
+};
