bab685954c36dabc856fc6f817c3ba0c075e2246
gxsoar <2278926493gx@gmail.com>
2023-05-01 08:54:32 +0800

    add gettime

---
+++ include/gettime.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifdef __APPLE__
+typedef enum {
+  CLOCK_REALTIME,
+  CLOCK_MONOTONIC,
+  CLOCK_PROCESS_CPUTIME_ID,
+  CLOCK_THREAD_CPUTIME_ID
+} clockid_t;
+
+int clock_gettime(clockid_t clk_id, struct timespec *tp);
+#endif
